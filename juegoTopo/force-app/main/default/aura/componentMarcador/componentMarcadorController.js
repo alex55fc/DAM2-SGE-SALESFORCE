d1267213a2082f2e3c880b27fffefaba856d11f3
alexa <alexander.55.fc@gmail.com>
2024-02-05 10:26:23 +0100

    Saving score in Salesforce Database

--- juegoTopo/force-app/main/default/aura/componentMarcador/componentMarcadorController.js
+++ juegoTopo/force-app/main/default/aura/componentMarcador/componentMarcadorController.js
@@ -13,5 +13,23 @@
         }
         //con componen.set se cambia el valor de la variable marcador y se actualiza en la vista
         component.set("v.marcador", marcador);
-    }
+    },
+    guardarScore : function(component, event, helper) {
+        var insertScoreApex = component.get("c.insertScore");
+
+        insertScoreApex.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+                alert("exito")
+            }
+        });
+
+        insertScoreApex.setParams({
+             "nombre": "vuestronombre",
+             "puntuacion": component.get("v.marcador")
+         });
+        //aqui se ejecuta la llamada a la clase de apex que inserta el score
+        $A.enqueueAction(insertScoreApex);
+
+    },
 })
